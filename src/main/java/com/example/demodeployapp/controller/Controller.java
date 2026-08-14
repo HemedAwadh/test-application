162c366dbc8d5211dfd85edd5e2f708c415a7586
Hemed Awadh <hemedawadh03@gmail.com>
2026-08-14 15:37:45 +0300

    added logs

--- src/main/java/com/example/demodeployapp/controller/Controller.java
+++ src/main/java/com/example/demodeployapp/controller/Controller.java
@@ -1,5 +1,6 @@
 package com.example.demodeployapp.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,11 +11,13 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1")
+@Slf4j
 public class Controller {
 
     String message = "Hello, this is my first Deployed Application";
     @PostMapping("/hello")
     public ResponseEntity<?> hello(){
+        log.info("*****Received Request****");
         return new ResponseEntity<>(Map.of("message",message),HttpStatus.OK);
     }
 
